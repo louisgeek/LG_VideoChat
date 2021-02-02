5ec97f03b605a83d4c3ac4540380411cdf255567
louisgeek <louisgeek@qq.com>
2021-02-02 11:19:36 +0800

    update

--- chat/src/main/java/com/louisgeek/chat/helper/ChatHelper.java
+++ chat/src/main/java/com/louisgeek/chat/helper/ChatHelper.java
@@ -5,7 +5,6 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.louisgeek.chat.model.ChatModel;
-import com.louisgeek.chat.model.UserModel;
 import com.louisgeek.chat.socketio.ChatEvents;
 import com.louisgeek.chat.socketio.SocketIOEvent;
 
@@ -16,21 +15,22 @@ public class ChatHelper {
     private static final String TAG = "ChatHelper";
     private static final Gson mGson = new Gson();
 
-    public static void doInvite(Context context, UserModel userModel, UserModel otherUserModel, ChatModel chatModel, OnInviteBack onInviteBack) {
+    public static void doInvite(Context context, ChatModel chatModel, OnInviteBack onInviteBack) {
+        //对讲生命周期开始
+        ChatUtil.userModel = chatModel.fromUserModel;
+        ChatUtil.otherUserModel = chatModel.toUserModel;
         //
-        ChatUtil.online(otherUserModel, new ChatUtil.OnlineBack() {
+        ChatUtil.online(ChatUtil.otherUserModel, new ChatUtil.OnlineBack() {
             @Override
             public void online(boolean isOnline) {
                 if (!isOnline) {
-                    Toast.makeText(context, otherUserModel.userName + "不在线", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, ChatUtil.otherUserModel.userName + "不在线", Toast.LENGTH_SHORT).show();
                    /* if (onInviteBack != null) {
                         onInviteBack.onOffline(otherUserModel);
                     }*/
                     return;
                 }
-                //对讲生命周期开始
-                ChatUtil.userModel = userModel;
-                ChatUtil.otherUserModel = otherUserModel;
+
                 //邀请
                 ChatUtil.sendInvite(chatModel);
                 //type 1
@@ -47,12 +47,12 @@ public class ChatHelper {
 
     public static void onInvite(SocketIOEvent socketIOEvent, OnInviteBack onInviteBack) {
         String event = socketIOEvent.event;
-        String chatInfoModelJson = socketIOEvent.json;
+        String chatModelJson = socketIOEvent.json;
         if (!ChatEvents.invite.equals(event)) {
             //其他呼叫消息 里面的页面处理
             return;
         }
-        ChatModel chatModel = mGson.fromJson(chatInfoModelJson, ChatModel.class);
+        ChatModel chatModel = mGson.fromJson(chatModelJson, ChatModel.class);
         //对讲生命周期开始
         ChatUtil.userModel = chatModel.toUserModel;
         ChatUtil.otherUserModel = chatModel.fromUserModel;
